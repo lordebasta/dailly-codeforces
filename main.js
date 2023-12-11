85cabae072093f219c534e34659e150528ca3f9a
LoSalv <41568059+lordebasta@users.noreply.github.com>
2023-12-11 22:02:57 +0100

    set the seed for problem decision
    set the chosen problem to be the same for the day if clicked multiple times

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
+function jsf32(a, b, c, d) {
+    return function () {
+        a |= 0; b |= 0; c |= 0; d |= 0;
+        var t = a - (b << 27 | b >>> 5) | 0;
+        a = b ^ (c << 17 | c >>> 15);
+        b = c + d | 0;
+        c = d + t | 0;
+        d = a + t | 0;
+        return (d >>> 0) / 4294967296;
+    }
+}
+
+function getProblemsUnderRanking(problems, max_ranking) {
+    let result = []
+    for (var x in problems) {
+        if (problems[x].rating < max_ranking) {
+            result.push(problems[x])
+        }
+    }
+    return result
+}
+
 fetch('https://codeforces.com/api/problemset.problems').then(r => r.text()).then(result => {
     // Result now contains the response text, do what you want...
     result = JSON.parse(result)
 
     problems = result.result.problems
-    candidates = []
+    let candidates = getProblemsUnderRanking(problems, 1000)
 
-    for (var x in problems) {
-        if (problems[x].rating < 1000) {
-            candidates.push(problems[x])
-        }
-    }
+    let date = new Date()
+    let rand = jsf32(date.getFullYear(), date.getMonth(), date.getDay(), date.getDate())
 
-    var item = candidates[Math.floor(Math.random() * candidates.length)];
-    console.log(item)
+    var item = candidates[Math.floor(rand() * candidates.length)];
     window.open("https://codeforces.com/problemset/problem/" + item.contestId + "/" + item.index, "_blank")
 
 })
