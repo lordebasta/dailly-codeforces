87ba3ed90d91188b96ac5e9d8a1430ac5ab361cb
LoSalv <41568059+lordebasta@users.noreply.github.com>
2023-12-21 13:34:40 +0100

    fix seed for random problem

--- main.js
+++ main.js
@@ -10,6 +10,24 @@ function jsf32(a, b, c, d) {
     }
 }
 
+function cyrb128(str) {
+    let h1 = 1779033703, h2 = 3144134277,
+        h3 = 1013904242, h4 = 2773480762;
+    for (let i = 0, k; i < str.length; i++) {
+        k = str.charCodeAt(i);
+        h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+        h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+        h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+        h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+    }
+    h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+    h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+    h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+    h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+    h1 ^= (h2 ^ h3 ^ h4), h2 ^= h1, h3 ^= h1, h4 ^= h1;
+    return [h1 >>> 0, h2 >>> 0, h3 >>> 0, h4 >>> 0];
+}
+
 function getProblemsInRanking(problems, min_ranking, max_ranking) {
     let result = []
     for (var x in problems) {
@@ -30,10 +48,13 @@ fetch('https://codeforces.com/api/problemset.problems').then(r => r.text()).then
         (items) => {
             let candidates = getProblemsInRanking(problems, items.mini, items.maxi)
             let date = new Date()
-            let rand = jsf32(date.getFullYear(), date.getMonth(), date.getDay(), date.getDate())
+            let seed = cyrb128(date.toString())
+            let rand = jsf32(seed[0], seed[1], seed[2], seed[3])
+            let index = rand()
+            index = Math.floor(index * candidates.length)
+            console.log(index)
             console.log(candidates)
-            var item = candidates[Math.floor(rand() * candidates.length)];
-            console.log(candidates[item])
+            let item = candidates[index];
             window.open("https://codeforces.com/problemset/problem/" + item.contestId + "/" + item.index, "_blank")
         }
     );
